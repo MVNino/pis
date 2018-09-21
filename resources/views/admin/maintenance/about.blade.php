e7589b2d2bcc1cfaab102a78897d66b20d0866d1
lhexyromero <lhexyromero@gmail.com>
2018-09-21 19:43:10 +0800

    Admin Side#4

--- resources/views/admin/maintenance/about.blade.php
+++ resources/views/admin/maintenance/about.blade.php
@@ -12,8 +12,6 @@
         </ol>
     </div>
 @endsection
-
-
 @section('content')
         {!! Form::open(['action' => 'Maintenance\AboutController@addAbout','class' => 'form-material' ,'method' => 'POST', 'autocomplete' => 'off', 'enctype' => 'multipart/form-data'])!!}
             <div class="row">
