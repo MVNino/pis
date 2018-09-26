a5b9f77711f079b396a529bd14ffd7552a0e1b28
Marlon Nino <me@ninomarlonvilla@gmail.com>
2018-09-26 13:39:51 +0800

    to commit

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -14,18 +14,20 @@ use App\OtherService;
 use App\SpecialtyService;
 
 class GuestController extends Controller 
-{   
-    public function _construct() 
-    {
-    }
-
+{ 
     public function viewIndex()
     {
-        $otherServices = OtherService::orderBy('other_services_id', 'desc')
+        $otherServices = OtherService::where('status', 1)
+                ->orderBy('other_services_id', 'desc')
                 ->limit(3)
                 ->get();
-        $news = News::orderBy('news_id', 'desc')->limit(3)->get();
-        $banners = Banner::where('banner_status', '=', 1)->get();
+        $news = News::where('status', 1)
+                ->orderBy('news_id', 'desc')
+                ->limit(3)
+                ->get();
+        $banners = Banner::where('banner_status', 1)
+                ->where('status', 1)
+                ->get();
         return view('guest.index', ['banners'=>$banners, 
                 'news' => $news, 'otherServices' => $otherServices]);
     }
--- resources/views/guest/show-service.blade.php
+++ resources/views/guest/show-service.blade.php
@@ -4,23 +4,25 @@
 
 <section class="speacial-services-area section-padding">
 <div class="row">
-    <div class="container">
-        <div class="col-sm-4">
-            <div class="speacila-single-service">
-                <div class="service-thumb">
-                    <img src="/storage/images/service/specialty/{{ $service->spec_image_icon }}" alt="Pic" width="300" height="250">
-                </div>
-                <h4>{{ $service->spec_title }}</h4>
-                <p>{{ $service->spec_desc }}</p>
-            </div>
-        </div>
-        <div class="col-md-8">
-            <iframe width="100%" height="400" 
-            src="{{ $service->specialtyServiceVid->video }}" 
-            frameborder="0" allowfullscreen>
-            </iframe>
-        </div>
-    </div>
+  <div class="container">
+      <div class="col-sm-4">
+          <div class="speacila-single-service">
+              <div class="service-thumb">
+                  <img src="/storage/images/service/specialty/{{ $service->spec_image_icon }}" alt="Pic" width="300" height="250">
+              </div>
+              <h4>{{ $service->spec_title }}</h4>
+              <p>{{ $service->spec_desc }}</p>
+          </div>
+      </div>
+      <div class="col-md-8">
+          @foreach($service->specialtyServiceVids as $specialtyServiceVid)
+          <iframe width="100%" height="400" 
+          src="{{ $specialtyServiceVid->video }}" 
+          frameborder="0" allowfullscreen>
+          </iframe>
+          @endforeach
+      </div>
+  </div>
 </div>
 </section>
 @endsection
