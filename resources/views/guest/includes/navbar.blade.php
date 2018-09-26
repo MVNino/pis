ac8a48a67e31dbe78eecae66bb1cbca35c33dc41
Marlon Nino <me@ninomarlonvilla@gmail.com>
2018-09-26 17:32:57 +0800

    login navbar and title

--- resources/views/guest/includes/navbar.blade.php
+++ resources/views/guest/includes/navbar.blade.php
@@ -3,9 +3,9 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <div class="col-sm-3 col-md-4">
                 <div class="site-logo" style="padding-top: 8px; padding-bottom: 5px;">
-                <img src="/public/images/logo/{{ Auth::user()->company_clinic_logo }}" style=" width: 300px; height: 50px;">
+                    <img src="/public/images/logo/{{ Auth::user()->company_clinic_logo }}" style=" width: 300px; height: 50px;">
+                </div>
             </div>
-        </div>
             <ul class="nav menu navbar-right navbar-nav">
                 <li id ="navlink-index"><a href="/"> Home </a></li>
                 <li id ="navlink-about"><a href="/about"> About </a></li>
