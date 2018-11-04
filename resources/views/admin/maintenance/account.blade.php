ba64b7e1751719c0f0a33232a233cb3c814ad6dc
Marlon Nino <me@ninomarlonvilla@gmail.com>
2018-11-05 00:20:29 +0800

    billing done/user-account done

--- resources/views/admin/maintenance/account.blade.php
+++ resources/views/admin/maintenance/account.blade.php
@@ -28,7 +28,7 @@
                         <div class="form-control" data-trigger="fileinput"> <i class="glyphicon glyphicon-file fileinput-exists"></i>
                             <span class="fileinput-filename"></span></div> <span class="input-group-addon btn btn-default btn-file">
                         <span class="fileinput-new">Select file</span> <span class="fileinput-exists">Change</span>
-                        <input type="hidden"><input type="file" name="bannerImage" accept=".jpg,.jpeg,.png"> </span> <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
+                        <input type="hidden"><input type="file" name="profileImage" accept=".jpg,.jpeg,.png"> </span> <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
                     </div>
                 </div>
                 <div class="form-group">
@@ -48,7 +48,8 @@
             <div id="pnlSecurity">
                 <h3 class="box-title" id="security">Change Account Password</h3>
                 {!! Form::open(['action' => ['Maintenance\AccountController@changePassword', Auth::user()->id], 'method' => 'POST', 
-                    'enctype' => 'multipart/form-data', 'onsubmit' => "return confirm('Change your password?')", 'class' => 'form-material form-horizontal', 'style' => 'padding-left:80px; padding-right:80px;']) !!}
+                    'onsubmit' => "return confirm('Change your password?')", 'class' => 'form-material form-horizontal', 
+                    'style' => 'padding-left:80px; padding-right:80px;']) !!}
                     @csrf
                     <div class="form-group">
                         <label class="col-md-12">Current Password</span></label>
